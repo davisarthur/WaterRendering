368fdf9abe451ac66c6e15dc7366d39b7fc8908c
davisarthur <dsarthur123@gmail.com>
2021-11-20 12:48:44 -0500

    updates

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -1,14 +1,15 @@
 #include <vector>
 #include <math.h>
 #include <random>
+#include <complex>
 #include <glm/glm.hpp>
 #include "glm/ext.hpp"
+#include "helperFunctions.h"
 using namespace std;
 
-float GRAVITY = 9.81f;
 float phillips_spectrum(float amplitude, glm::vec2 wave_vector, glm::vec2 wind_vector) {
-    float k = glm::magnitude(wave_vector);
-    float V = glm::magnitude(wind_vector);
+    float k = glm::length(wave_vector);
+    float V = glm::length(wind_vector);
     float L = pow(V, 2.0) / GRAVITY;
     float dot_product = glm::dot(glm::normalize(wave_vector), glm::normalize(wind_vector));
     return amplitude * exp(-1.0 / pow(k * L, 2.0)) / pow(k, 4.0) * pow(dot_product, 2.0);
@@ -22,4 +23,34 @@ glm::vec2 height_fourier_space(glm::vec2 wave_vector, float Ph) {
     float real = 1.0 / pow(2.0, 0.5) * ran1 * pow(Ph, 0.5);
     float imaginary = 1.0 / pow(2.0, 0.5) * ran2 * pow(Ph, 0.5);
     return glm::vec2(real, imaginary);
+}
+
+vector<complex<float> > fft(vector<complex<float> > &input, float sign) {
+    int N = input.size();
+    if (N == 1) {
+        return input;
+    }
+    else {
+        pair<vector<complex<float> >, vector<complex<float> > > even_odd = even_odd_split(input);
+        vector<complex<float> > even = fft(even_odd.first, sign);
+        vector<complex<float> > odd = fft(even_odd.second, sign);
+        vector<complex<float> > output(N, 0.0f);
+        for (int k = 0; k < N / 2; k++) {
+            complex<float> p = even.at(k);
+            complex<float> q = odd.at(k) * complex<float>(cos(-2 * M_PI / (N * k)), sin(-2 * M_PI / (N * k)));
+            output[k] = p + q;
+            output[k + N / 2] = p - q;
+        }
+        return output;
+    }
+}
+
+pair<vector<complex<float> >, vector<complex<float> > > even_odd_split(vector<complex<float> > &input) {
+    vector<complex<float> > even;
+    vector<complex<float> > odd;
+    for (int i = 0; i < input.size() / 2; i++) {
+        even.push_back(input.at(i));
+        odd.push_back(input.at(i + 1));
+    }
+    return pair<vector<complex<float> >, vector<complex<float> > >(even, odd);
 }
--- helperFunctions.h
+++ helperFunctions.h
@@ -4,6 +4,10 @@
 #include "glm/ext.hpp"
 using namespace std;
 
+float GRAVITY = 9.81f;
+float BACKWARD_FFT = -1.0f;
+float FORWARD_FFT = 1.0f;
+
 /*
 
 */
@@ -22,3 +26,14 @@ Returns:
     height map value at t = 0 in Fourier space
 */
 glm::vec2 height_fourier_space(glm::vec2 wave_vector, float Ph);
+
+pair<vector<complex<float> >, vector<complex<float> > > even_odd_split(vector<complex<float> > &input);
+
+/*
+Requires:
+    input - complex vector
+    sign - direction of the transform
+Returns:
+    output - complex vector
+*/
+vector<complex<float> > fft(vector<complex<float> > &input, float sign);
